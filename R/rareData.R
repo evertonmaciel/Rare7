ef43b49f6f53bb0cad852c60de7b8e5558356bf4
evertonmaciel <everttonmaciel@gmail.com>
2019-07-20 17:02:16 -0300

    funções

---
+++ R/rareData.R
@@ -0,0 +1,42 @@
+#' Table data colection
+#'
+#' quantifies geographic range, abundance and habitat number for each species
+#'
+#' @param  table containing the information necessary to calculate Rabinowitz forms of rarity
+#' @return The output is another table organised to be used in the function rarityForms
+#' @export
+rareData <- function(table){
+  `%ni%` <- Negate(`%in%`)
+  Species <- as.character(sort(unique(table$specie)))
+  Sample_area <- round(abs(diff(range(table$lat))))
+  Detection_area <- numeric()
+  for(i in 1:length(Species))
+  {
+    Detection_area[i] <- length(unique(round(table$lat[which(table$specie==Species[i])])))
+  }
+  Abundance <- numeric()
+  for(i in 1:length(Species))
+  {
+    a <- table$NumIndiv[which(table$specie==Species[i])]
+    b <- max(a)
+    if(b!=0){
+      Abundance[i] <- b
+    }else{
+      Abundance[i] <- NA
+    }
+  }
+  Habitats <- numeric()
+  for(i in 1:length(Species))
+  {
+    a <- unique(table$habitat[which(table$specie==Species[i])])
+    b <- which(a==0)
+    if(length(b)==0){
+      Habitats[i] <- length(a)
+    }else{
+      Habitats[i] <- length(a[-b])
+    }
+  }
+  Habitats[which(Habitats==0)] <- NA
+  table2 <- data.frame(Species,Sample_area,Detection_area,Abundance,Habitats)
+  return(table2)
+}

f62f76196f215d789a215eecf4a31dc84ff8ded1
Eduardo Arlé <eduardo.arle@idiv.de>
2019-07-26 14:44:08 +0200

    rarePlot

--- R/rarePlot.R
+++ R/rarePlot.R
@@ -2,7 +2,7 @@
 #'
 #' plots a pie chart showing the shares of rarity forms in a specif locality
 #'
-#' @import data.table
+#' @importfrom data.table
 #' @param  table containing the information of species occurrence per locality
 #' @param  rarity the output provided by the function rareForms
 #' @param  locality character, name of the target locality
@@ -10,9 +10,23 @@
 #' @export
 rarePlot <- function (table,rarity,locality) 
 {
-  table$CodSite <- as.character(table$CodSite)
-  loc <- data.table::unique(data.table::as.data.table(table), by = "CodSite")
-  return(loc)
+  loc <- unique(table$CodSite)
+  sps_loc <- list()
+  for (i in 1:length(loc)) {
+    sps_loc[[i]] <- table$specie[which(table$CodSite == loc[i])]
+    names(sps_loc)[[i]] <- as.character(loc[i])
+  }
+  sps_loc_rarity <- list()
+  for (i in 1:length(sps_loc)) {
+    sps_loc_rarity[[i]] <- character()
+    for (j in 1:length(sps_loc[[i]])) {
+      sps_loc_rarity[[i]][j] <- as.character(rarity$Forma[which(rarity$Species == 
+                                                                  sps_loc[[i]][j])])
+    }
+    names(sps_loc_rarity)[[i]] <- as.character(loc$CodSite[i])
+  }
+  loc_tables <- lapply(lapply(sps_loc_rarity, table), data.frame)
+  pie(loc_tables[[which(names(loc_tables) == locality)]]$Freq, 
+      labels = loc_tables[[which(names(loc_tables) == locality)]]$Var1, 
+      main = names(loc_tables[which(names(loc_tables) == locality)]))
 }
-?unique
-library(data.table)

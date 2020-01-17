8ddab066da31a1a066351cb2a02c077381276cdf
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-01-17 08:56:07 -0500

    review rarePlot()

--- R/rarePlot.R
+++ R/rarePlot.R
@@ -1,27 +1,26 @@
 #' Local forms of rarity
 #'
-#' plots a pie chart showing the shares of rarity forms in a specif locality
+#' plots a pie chart showing the shares of rarity forms in a specific locality.
 #'
-#' @importfrom data.table
-#' @param  table containing the information of species occurrence per locality
-#' @param  rarity the output provided by the function rareForms
-#' @param  locality character, name of the target locality
+#' @param table containing the information of species occurrence per locality
+#' @param rarity the output provided by the function rareForms
+#' @param locality character, name of the target locality
 #' @return The function plots a pie chart showing the proportion of each rarity form for a target locality
 #' @export
-rarePlot <- function (table,rarity,locality) 
+rarePlot <- function(table, rarity, locality) 
 {
   loc <- unique(table$CodSite)
   sps_loc <- list()
-  for (i in 1:length(loc)) {
+  for (i in seq_along(loc)) {
     sps_loc[[i]] <- table$specie[which(table$CodSite == loc[i])]
     names(sps_loc)[[i]] <- as.character(loc[i])
   }
   sps_loc_rarity <- list()
-  for (i in 1:length(sps_loc)) {
+  for (i in seq_along(sps_loc)) {
     sps_loc_rarity[[i]] <- character()
-    for (j in 1:length(sps_loc[[i]])) {
-      sps_loc_rarity[[i]][j] <- as.character(rarity$Form[which(rarity$Species == 
-                                                                  sps_loc[[i]][j])])
+    for (j in seq_along(sps_loc[[i]])) {
+      sps_loc_rarity[[i]][j] <- as.character(rarity$Form[which(
+        rarity$Species == sps_loc[[i]][j])])
     }
     names(sps_loc_rarity)[[i]] <- as.character(loc[i])
   }
@@ -29,4 +28,5 @@ rarePlot <- function (table,rarity,locality)
   pie(loc_tables[[which(names(loc_tables) == locality)]]$Freq, 
       labels = loc_tables[[which(names(loc_tables) == locality)]]$Var1, 
       main = names(loc_tables[which(names(loc_tables) == locality)]))
+  invisible(NULL)
 }

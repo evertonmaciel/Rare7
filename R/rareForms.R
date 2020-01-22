716a350ca70dca4ffcebd909a36558647b4a78af
Eduardo Arlé <eduardo.arle@idiv.de>
2020-01-22 16:15:58 +0100

    Test unit for rarePlot

--- R/rareForms.R
+++ R/rareForms.R
@@ -6,7 +6,7 @@
 #' @param percentage the percentage of localities below which the species will fulfil the first parametre to be considered rare. Default is 10%, but the user can change it.
 #' @param min_abund the abundace  above which the species will fulfil the second parametre to be considered rare. Default is 2, but the user can change it.
 #' @param habitats the number of habitats above which the species will fulfil the third parametre to be considered rare. Default is 1, but the user can change it.
-#' @return The output is another table organised to be used in the function rarityForms
+#' @return The output is the result table showing the name of each species in the first column and the form of rarity (or common) in the second column.
 #' @export
 rareForms <- function(data, percentage=0.1, min_abund=2 ,habitats=1){
   `%ni%` <- Negate(`%in%`) #create an operator for "not in"
---
+++ tests/testthat/test-rarePlot.R
@@ -0,0 +1,13 @@
+library(testthat)
+library(checkmate)
+context("rarePlot")
+
+input_data1 <- read.csv(system.file("inst/test_datasets/Test_data.csv",package="Rare7",mustWork = T))
+input_data2 <- read.csv(system.file("inst/test_datasets/Table_for_rarePlot.csv",package="Rare7",mustWork = T))
+locality <- "Site6"
+  
+test_that("Expected data structure",{
+  expect_equal(class(input_data1),"data.frame")
+  expect_equal(class(input_data2),"data.frame")
+  expect_equal(class(locality),"character")
+})
